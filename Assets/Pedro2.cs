18a9aeb19ed037e942a0144390222cdd241eab3f
Endika Fernandez <fernandez.endika@opendeusto.es>
2024-02-23 08:48:21 +0100

    namespace

---
+++ Assets/Pedro2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace pedro
+{
+    public class Pedro: MonoBehaviour
+    {
+        void start()
+        {
+            Debug.Log("Pedro 2");
+        }
+    }
+}
+
