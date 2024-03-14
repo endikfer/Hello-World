1e552c524478153e7335003ca391c8ca81f1f7b9
Endika Fernandez <fernandez.endika@opendeusto.es>
2024-03-14 08:15:44 +0100

    cambios

--- Assets/Color tag.cs
+++ Assets/Color tag.cs
@@ -1,22 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Colortag : MonoBehaviour
 {
-    public GameObject cubo;
+    public GameObject[] objects;
     public Color color1;
+    public Boolean FindByTag;
+    public string ChoosenTag;
+
     // Start is called before the first frame update
     void Start()
     {
-        cubo = GameObject.FindWithTag("objeto");
-        color1 = Color.cyan;
-        cubo.GetComponent<MeshRenderer>().material.color = color1;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        colorObjeto();
+    }
+
+    private void colorObjeto()
+    {
+        if (FindByTag == true)
+        {
+            if (ChoosenTag != null && ChoosenTag != "")
+            {
+                objects = GameObject.FindGameObjectsWithTag(ChoosenTag);
+                for (int i = 0; i < objects.Length; i++)
+                {
+                    objects[i].GetComponent<MeshRenderer>().material.color = color1;
+                }
+            }
+        }
     }
 }
